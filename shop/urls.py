35507319cd6dbeea2a4acb9704505fcfd5ce1a64
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-04-05 09:12:45 +0300

    :tada: first commit

---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+
+app_name = 'shop'
+
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
