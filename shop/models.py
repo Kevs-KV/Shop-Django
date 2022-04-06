9bc8fab0789803a7c32156e9f18406c4bccbfc21
Kevs <k.vashhenko@gmail.com>
2022-04-06 21:47:08 +0300

    :pencil2: fix name

--- shop/models.py
+++ shop/models.py
@@ -64,5 +64,5 @@ class Comment(models.Model):
         ordering = ('created',)
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.name, self.post)
+        return 'Comment by {} on {}'.format(self.name, self.product)
 
