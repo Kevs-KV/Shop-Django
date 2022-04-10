045df8f99487ad243513e4c5d7f8703d4f22ba2e
Kevs <k.vashhenko@gmail.com>
2022-04-10 21:57:18 +0300

    :construction: add url comments

--- shop/urls.py
+++ shop/urls.py
@@ -10,7 +10,11 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<int:id>/<slug:slug>/comments/', views.product_comment, name='product_comment'),
 ]
+
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
