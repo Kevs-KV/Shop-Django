e6185b327df3f44722163e9b801967c0897c2535
Kevs <k.vashhenko@gmail.com>
2022-04-10 21:56:16 +0300

    :construction: add separation detail and comments

--- shop/views.py
+++ shop/views.py
@@ -16,22 +16,34 @@ def product_list(request, category_slug=None):
                    'categories': categories,
                    'products': products})
 
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     gallery = Gallery.objects.filter(product=id)
     comments = product.comments.filter(active=True)
+    return render(request, 'shop/detail.html',
+                  {'product': product, 'gallery': gallery, 'comments': comments})
+
+
+def product_comment(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    comments = product.comments.filter(active=True)
+    gallery = Gallery.objects.filter(product=id)
     new_comment = None
-    if request.method == 'POST':
+    # Show 25 contacts per page.
+    if request.method == 'POST' and 'add_comment' in request.POST:
         # Пользователь отправил комментарий.
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             # Создаем комментарий, но пока не сохраняем в базе данных.
             new_comment = comment_form.save(commit=False)
+            new_comment.product_id = id
             # Привязываем комментарий к текущей статье.
             new_comment.post = product
             # Сохраняем комментарий в базе данных.
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'shop/product.html',
-                  {'product': product, 'gallery': gallery, 'comment_form': comment_form, 'comments': comments})
+    return render(request, 'shop/comments.html',
+                  {'product': product, 'gallery': gallery, 'comment_form': comment_form,
+                   'comments': comments})
