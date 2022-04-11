50240fcdfb9019ed01b0f3a42626285eb82dcc20
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-04-11 16:57:41 +0300

    :construction: add crispy_forms

--- shop/forms.py
+++ shop/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Div, HTML
 from django import forms
 
 from .models import Comment
@@ -7,11 +9,28 @@ class CommentForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'type': 'text', 'placeholder': "Your Name"}))
     email = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'type': 'email', 'placeholder': "Your Email"}))
     body = forms.CharField(widget=forms.Textarea(attrs={'class': 'input', 'placeholder': "Your Review"}))
-    star1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star1', 'value': 1, }))
-    star2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star2', 'value': 2, }))
-    star3 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star3', 'value': 3, }))
-    star4 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star4', 'value': 4, }))
-    star5 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star5', 'value': 5, }))
+    star = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'id': 'star' }))
+    # star1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star1', 'value': 1, }))
+    # star2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star2', 'value': 2, }))
+    # star3 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star3', 'value': 3, }))
+    # star4 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star4', 'value': 4, }))
+    # star5 = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'name': 'rating', 'type': 'radio', 'id': 'star5', 'value': 5, }))
+
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'name',
+            'email',
+            'body',
+            Div(HTML('<span>Your Rating: </span>'),
+                Div('star',
+                    css_class='stars'),
+                HTML('{% csrf_token %}'),
+                Submit('Submit', 'submit', css_class="primary-btn"),
+                css_class='input-rating'
+            ))
