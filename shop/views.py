770edee2292d5d86e0bd8facfd36066faf09396b
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-04-15 16:22:23 +0300

    :construction: replace product_list to ViewProductList

--- shop/views.py
+++ shop/views.py
@@ -2,23 +2,36 @@ from collections import Counter
 
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 
 from .forms import CommentForm
 from .models import Category, Product, Gallery, Comment
 
 
-def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request, 'shop/index.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products})
+# def product_list(request, category_slug=None):
+#     category = None
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True)
+#     if category_slug:
+#         category = get_object_or_404(Category, slug=category_slug)
+#         products = products.filter(category=category)
+#     return render(request, 'shop/index.html',
+#                   {'category': category,
+#                    'categories': categories,
+#                    'products': products})
+
+class ViewProductList(ListView):
+    template_name = 'shop/index.html'
+    model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, *kwargs)
+        context['categories'] = Category.objects.all()
+        print(context)
+        return context
+
+    def get_queryset(self):
+        return Product.objects.filter(available=True)
 
 
 def product_detail(request, id, slug):
