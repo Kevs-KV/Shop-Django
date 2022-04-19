7b8fb9fb3973d2d1fdd99e50f042fe2f52500be5
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-04-19 16:42:26 +0300

    :construction: add Brand

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Gallery, Comment
+from .models import Category, Product, Gallery, Comment, Brand
 
 
 @admin.register(Category)
@@ -7,10 +7,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
-    list_filter = ['available', 'created', 'updated']
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'brand']
+    list_filter = ['available', 'created', 'updated', 'brand']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
 
--- shop/models.py
+++ shop/models.py
@@ -19,12 +19,22 @@ class Category(models.Model):
         return f'category/{self.name}'
 
 
+class Brand(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category,
                                  related_name='products',
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
+    brand = models.ForeignKey(Brand,
+                              related_name='brands', null=True,
+                              on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='products', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -43,12 +53,9 @@ class Product(models.Model):
         return reverse('shop:product_detail', args=[self.id, self.slug])
 
 
-
 class Gallery(models.Model):
     image = models.ImageField(upload_to='gallery')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-
-
 
 
 class Comment(models.Model):
@@ -61,10 +68,8 @@ class Comment(models.Model):
     active = models.BooleanField(default=True)
     rating = models.IntegerField(default=0)
 
-
     class Meta:
         ordering = ('created',)
 
     def __str__(self):
         return 'Comment by {} on {}'.format(self.name, self.product)
-
