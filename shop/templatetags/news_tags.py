e7c61fdf6ac056142373f662364c63b9cceb4bca
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-04-28 17:11:15 +0300

    :construction: get news tag for get categories

--- shop/templatetags/news_tags.py
+++ shop/templatetags/news_tags.py
@@ -1,11 +1,16 @@
 from django import template
 
 from cart.cart import Cart
+from shop.models import Category
 
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
+@register.simple_tag(takes_context=True, name='get_cart')
 def get_cart(context):
     request = context['request']
     return Cart(request)
+
+@register.simple_tag(name='get_category')
+def get_category():
+    return Category.objects.all()
