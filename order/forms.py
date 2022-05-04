b90c732c9899d3c69c3ba838d169f8db72fd0ca5
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-04 16:09:16 +0300

    :construction: add view and form order

---
+++ order/forms.py
@@ -0,0 +1,8 @@
+from order.models import Order
+from django import forms
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'city', 'country', 'telephone']
--- order/views.py
+++ order/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import FormView
+
+from cart.cart import Cart
+from order.forms import OrderForm
+from order.models import Order, Item
+
+
+class ViewCreateOrder(FormView):
+    template_name = 'shop/checkout.html'
+    model = Order
+    form_class = OrderForm
+
+    def get_cart(self):
+        return Cart(self.request)
+
+    def get_context_data(self, **kwargs):
+        context = super(ViewCreateOrder, self).get_context_data(**kwargs)
+        context['cart'] = self.get_cart()
+        return context
+
+    def form_valid(self, form):
+        order = form.save(commit=False)
+        cart = self.get_cart()
+        for item in cart:
+            Item.objects.create(order=order,
+                                product=item['product'],
+                                price=item['price'],
+                                quantity=item['quantity'])
+        cart.clear()
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('shop:product_list')
