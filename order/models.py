79e34cf0ed6259c696afa0dea03bc23c1276cbd9
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-04 16:08:17 +0300

    :construction: fix model order

--- order/models.py
+++ order/models.py
@@ -9,7 +9,7 @@ class Order(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    adress = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     telephone = models.CharField(max_length=50)
@@ -22,17 +22,22 @@ class Order(models.Model):
         ordering = ('-creation_date',)
 
 
-
 class Item(models.Model):
-    cart = models.ForeignKey(Order, verbose_name='cart', on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name='quantity', default=1)
-    unit_price = models.DecimalField(max_digits=18, decimal_places=2, verbose_name='unit price')
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='item',
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items',
                                 on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
 
     class Meta:
         verbose_name = 'item'
         verbose_name_plural = 'items'
-        ordering = ('cart',)
-
+        ordering = ('order',)
