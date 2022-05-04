1142e601dd3b367c98274d670855fc4312fa84cc
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-04 16:08:39 +0300

    :construction: add admin app order

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from order.models import Order, Item
+
+
+class OrderItemInline(admin.TabularInline):
+    model = Item
+    raw_id_fields = ['product']
+
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ['first_name', 'last_name', 'email', 'address', 'city', 'country', 'telephone', 'creation_date', 'checked_out']
+    list_filter = ['creation_date', 'checked_out']
+    inlines = [OrderItemInline]
