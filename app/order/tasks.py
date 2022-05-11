b13f0e4d5a0a27b70a34d796bda13e046e345575
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-11 16:56:32 +0300

    :construction: fix task

--- app/order/tasks.py
+++ app/order/tasks.py
@@ -1,7 +1,7 @@
-from website.celery import app
+from django.conf import settings
 from django.core.mail import send_mail
 
-from website.settings import EMAIL_HOST_USER
+from website.celery import app
 from .models import Order
 
 
@@ -11,5 +11,5 @@ def order_created(order_id, mail):
     subject = 'Order nr. {}'.format(order.id)
     message = 'Dear {},\n\nYou have successfully placed an order.\ Your order id is {}.'.format(order.first_name,
                                                                                                 order.id)
-    mail_sent = send_mail(subject, message, EMAIL_HOST_USER, mail, fail_silently=False)
+    mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, (mail,), fail_silently=False)
     return mail_sent
