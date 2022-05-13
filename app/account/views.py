c5a9cd4bec526be183b4372808778662254760c8
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-13 16:14:28 +0300

    :construction: add reverse after authorization

--- app/account/views.py
+++ app/account/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-
+from account.forms import AuthenticationUserForm
 
 
 class LoginUser(LoginView):
     template_name = 'account/login.html'
+    form_class = AuthenticationUserForm
 
     def form_valid(self, form):
         """Security check complete. Log the user in."""
         auth_login(self.request, form.get_user())
-        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
+        return HttpResponseRedirect(reverse('shop:product_list'))
+
+
