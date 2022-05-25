e69c3732146d7e4552db931e86010a1d38f5826a
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-25 16:55:59 +0300

    :construction: add login url

--- app/website/settings.py
+++ app/website/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+LOGIN_URL = "/account/login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
