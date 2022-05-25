d443714d23bcfd8703a571fae170ab6773d40e9c
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-25 16:56:53 +0300

    :construction: add view number of products in wishlist

--- app/shop/templatetags/news_tags.py
+++ app/shop/templatetags/news_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 from cart.cart import Cart
 from shop.models import Category
+from wishlist.models import Wishlist
 
 register = template.Library()
 
@@ -11,6 +12,18 @@ def get_cart(context):
     request = context['request']
     return Cart(request)
 
+
 @register.simple_tag(name='get_category')
 def get_category():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True, name='get_products_in_wishlist')
+def get_amount_product(context):
+    request = context['request']
+    if request.user.is_authenticated:
+        return len(
+            Wishlist.objects.filter(user=request.user)
+        )
+    else:
+        return '0'
--- app/wishlist/models.py
+++ app/wishlist/models.py
@@ -8,5 +8,6 @@ class Wishlist(models.Model):
     user = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='products', on_delete=models.CASCADE)
 
+
     class Meta:
         ordering = ('user',)
