c6f5f45e07c75fb9d4610a7ac8cd47ed59421b57
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-05-25 16:55:41 +0300

    :construction: add login_required for wishlist

--- app/wishlist/views.py
+++ app/wishlist/views.py
@@ -1,21 +1,23 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView
 
 from shop.models import Product
+from shop.templatetags.news_tags import get_amount_product
 from wishlist.models import Wishlist
 
 
 class WishlistCreate(View):
     model = Wishlist
 
+    @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         product = get_object_or_404(Product, id=self.kwargs['product_id'], available=True)
-        if not self.request.user.is_authenticated:
-            return HttpResponseRedirect(reverse("account:login"))
         if not Wishlist.objects.filter(user=self.request.user, product=product):
             Wishlist.objects.create(user=self.request.user, product=product)
         return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
@@ -24,6 +26,7 @@ class WishlistCreate(View):
 class WishlistDelete(View):
     model = Wishlist
 
+    @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         product = get_object_or_404(Product, id=self.kwargs['product_id'], available=True)
         Wishlist.objects.filter(user=self.request.user, product=product).delete()
@@ -33,6 +36,16 @@ class WishlistDelete(View):
 class ViewWishList(ListView):
     model = Wishlist
     template_name = 'wishlist/wishlist.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(
+                self, request.method.lower(), self.http_method_not_allowed
+            )
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
 
     def get_queryset(self):
         return Wishlist.objects.filter(user=self.request.user)
