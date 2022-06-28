799a0b9548e6ff302872e450b19717ed26328a48
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-06-28 16:31:37 +0300

    :construction: add check activation new user

--- app/account/tasks.py
+++ app/account/tasks.py
@@ -6,6 +6,7 @@ from website.celery import app
 @app.task
 def send_email_password_reset(subject_template_name, email_template_name, context,
                               from_email, to_email, html_email_template_name):
+
     context['user'] = User.objects.get(pk=context['user'])
 
     return PasswordResetForm.send_mail(
@@ -17,3 +18,12 @@ def send_email_password_reset(subject_template_name, email_template_name, contex
         to_email,
         html_email_template_name
     )
+
+
+@app.task
+def check_activation_new_user(user_id):
+    user = User.objects.get(pk=user_id[0])
+    if user.is_active:
+        return True
+    user.delete()
+
