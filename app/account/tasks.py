c4e8794a48ee9615011a295ba549fd57d85bc116
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-06-29 16:56:00 +0300

    :bug: fix user id

--- app/account/tasks.py
+++ app/account/tasks.py
@@ -22,7 +22,7 @@ def send_email_password_reset(subject_template_name, email_template_name, contex
 
 @app.task
 def check_activation_new_user(user_id):
-    user = User.objects.get(pk=user_id[0])
+    user = User.objects.get(pk=user_id)
     if user.is_active:
         return True
     user.delete()
